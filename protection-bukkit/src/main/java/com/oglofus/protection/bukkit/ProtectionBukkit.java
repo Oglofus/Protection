bc860dc7d0cf47b403badab1003896dbe0176b35
nikosgram13 <nikosgram@windowslive.com>
2019-01-22 00:39:39 +0000

    Add travis ci configuration file

--- protection-bukkit/src/main/java/com/oglofus/protection/bukkit/ProtectionBukkit.java
+++ protection-bukkit/src/main/java/com/oglofus/protection/bukkit/ProtectionBukkit.java
@@ -18,7 +18,6 @@ package com.oglofus.protection.bukkit;
 
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.List;
